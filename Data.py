ce2e82b97af8d62fb9de8e0df8682ef491c04feb
Λ L E X <112969240+AL3X-Github@users.noreply.github.com>
2022-10-08 03:50:14 +0530

    Add files via upload

--- Data.py
+++ Data.py
@@ -2,14 +2,14 @@ from pyrogram.types import InlineKeyboardButton
 
 class Data:
     HELP = """
-<b> ❏ Commands For BOT Users 
+<b> 😊 Commands For BOT Users 
  ├ /start - Start Bot
  ├ /about - About This Bot
  ├ /help - This Bot Command Help
  ├ /ping - To Check Alive Bot
  └ /uptime - To View Bot Status
  
- ❏ Commands For BOT Admin
+ 😉️ Commands For BOT Admin
  ├ /logs - To View Bot Logs
  ├ /setvar - To Set Var With Command
  ├ /delvar - To Delete Var With Command
@@ -19,29 +19,29 @@ class Data:
  ├ /speedtest - To Test The Speed Of The Bot Server
  └ /broadcast - To Send Broadcast Messages To Bot Users
 
-👨‍💻 Develoved By : </b><a href='https://t.me/MaximXRobot/101'>I𝗓υɱi 和泉</a>
+👨‍💻 Develoved By : </b><a href='https://t.me/MaximXRobot'>I𝗓υɱi 和泉</a>
 """
 
     close = [
-        [InlineKeyboardButton("𝐂𝐥𝐨𝐬𝐞", callback_data="close")]
+        [InlineKeyboardButton("⛔ 𝐂𝐥𝐨𝐬𝐞 ⛔", callback_data="close")]
     ]
 
     mbuttons = [
         [
             InlineKeyboardButton("𝐇𝐞𝐥𝐩 & 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬", callback_data="help"),
-            InlineKeyboardButton("𝐂𝐥𝐨𝐬𝐞", callback_data="close")
+            InlineKeyboardButton("⛔ 𝐂𝐥𝐨𝐬𝐞 ⛔", callback_data="close")
         ],
     ]
 
     buttons = [
         [
             InlineKeyboardButton("𝐀𝐛𝐨𝐮𝐭 𝐌𝐞", callback_data="about"),
-            InlineKeyboardButton("𝐂𝐥𝐨𝐬𝐞", callback_data="close")
+            InlineKeyboardButton("⛔ 𝐂𝐥𝐨𝐬𝐞 ⛔", callback_data="close")
         ],
     ]
 
     ABOUT = """
-<b>About This Bot:
+<b>🤖 About This Bot :
 
 @{} Is A Telegram Bot To Store Posts Or Files That Can Be Accessed Via Special Links Work 24×7.
 
@@ -49,5 +49,5 @@ class Data:
  • Framework: <a href='https://docs.pyrogram.org'>Pyrogram</a>
  • Source Code: <a href='https://t.me/+vBu5aXlocTkwNGM1'>TG-FileShare-Bot</a>
 
-👨‍💻 Develoved By :</b><a href='https://t.me/MaximXRobot/101'>I𝗓υɱi 和泉</a>
+👨‍💻 Develoved By :</b><a href='https://t.me/MaximXRobot'>I𝗓υɱi 和泉</a>
 """
